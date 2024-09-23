f5efa59bab83e7f965f827bd730a1c98174791b5
7R0N1X <techsoftec.tronix@gmail.com>
2024-09-23 18:19:06 -0500

    feat: init project

---
+++ js/app.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const $date = document.querySelector("#date");
+  const $temperature = document.querySelector("#temperature > span");
+  const $city = document.querySelector("#city");
+  const $min = document.querySelector("#min");
+  const $max = document.querySelector("#max");
+  const $humidity = document.querySelector("#humidity");
+
+  const getDate = () => {
+    const date = new Date();
+
+    const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    const dayText = days[date.getDay()]
+    const dayNumber = date.getDate();
+    const monthText = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    return `${dayText}, ${dayNumber} de ${monthText}, ${year}`;
+  }
+
+  const getLocation = async () => {
+    const response = await fetch("https://ipinfo.io/json?token=06f1f115861750");
+    const data = await response.json();
+    return data
+  };
+
+  const kelvinToCelsius = (kelvin) => Math.floor(kelvin - 273.15);
+
+  const getTemperature = async (lat, lon) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c313a35c8ef04ea4a26f41b8f67d7b32`
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
+  getLocation().then(data => {
+    const { loc } = data
+    const [lat, lon] = loc.split(",");
+
+    getTemperature(lat, lon).then(data => {
+      const { main: { temp, temp_max, temp_min, humidity }, name } = data;
+      const _temp = kelvinToCelsius(temp);
+      const tempMax = kelvinToCelsius(temp_max);
+      const tempMin = kelvinToCelsius(temp_min);
+      const _humidity = humidity + "%";
+
+      $temperature.innerText = _temp;
+      $city.innerText = name;
+      $min.innerText = `Min: ${tempMin}`;
+      $max.innerText = `Max: ${tempMax}`;
+      $humidity.innerText = `Hum: ${_humidity}`;
+    })
+  })
+
+  $date.innerText = getDate();
+})
